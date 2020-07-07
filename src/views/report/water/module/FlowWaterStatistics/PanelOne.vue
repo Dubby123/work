273390e03362111365cbb98c0fcb1444abfa9120
wangdj <wangdj@citydo.com.cn>
2020-07-07 10:34:27 +0800

    流量水质统计

--- src/views/report/water/module/FlowWaterStatistics/PanelOne.vue
+++ src/views/report/water/module/FlowWaterStatistics/PanelOne.vue
@@ -1,17 +1,42 @@
 <!-- PanelOne -->
 <template>
   <rect-box class="_PanelOne_root">
-    <water-statistics />
+    <water-statistics :data="myData1" />
+    <div class="chart">
+      <water-line-chart />
+    </div>
+    <water-statistics class="item-last" :data="myData2" />
   </rect-box>
 </template>
 
 <script>
 import WaterStatistics from '../../components/WaterStatistics'
+import WaterLineChart from '../../components/WaterLineChart'
+import icon1 from "../../components/icon/icon-1-1.png";
+import icon2 from "../../components/icon/icon-1-2.png";
 
 export default {
   name: 'PanelOne',
   components: {
     WaterStatistics,
+    WaterLineChart,
+  },
+  data() {
+    return {
+      myData1: {
+        icon: icon1,
+        name: '当日累计进水总量',
+        value: 321256,
+        unit: '㎡',
+        rate: -8,
+      },
+      myData2: {
+        icon: icon2,
+        name: '当日累计排水总量',
+        value: 256256,
+        rate: 8,
+      },
+    };
   },
 };
 </script>
@@ -21,6 +46,18 @@ export default {
     position: relative;
     width: 393px;
     height: 444px;
-    padding-top: 20px;
+    padding: 20px 0;
+    display: flex;
+    flex-wrap: wrap;
+
+    .chart {
+      margin-top: 30px;
+      width: 100%;
+      height: 200px;
+    }
+
+    .item-last {
+      align-self: flex-end;
+    }
   }
 </style>
--- src/views/report/water/module/FlowWaterStatistics/PanelTwo.vue
+++ src/views/report/water/module/FlowWaterStatistics/PanelTwo.vue
@@ -1,17 +1,51 @@
 <!-- PanelOne -->
 <template>
   <rect-box class="_PanelOne_root">
-    <water-statistics />
+    <water-statistics :data="myData1" />
+    <water-statistics class="item-last" :data="myData2" />
   </rect-box>
 </template>
 
 <script>
 import WaterStatistics from '../../components/WaterStatistics'
+import icon1 from "../../components/icon/icon-2-1.png";
+import icon2 from "../../components/icon/icon-2-2.png";
 
 export default {
   name: 'PanelOne',
   components: {
     WaterStatistics,
+  },
+  data() {
+    return {
+      myData1: {
+        icon: icon1,
+        name: '生活污水合计进水量',
+        value: 93041,
+        unit: '㎡',
+        rate: -8,
+        other: [
+          {
+            name: '市排1400进水量',
+            num: '33,039',
+          },
+          {
+            name: '新齐贤泵站',
+            num: '30,001',
+          },
+          {
+            name: '安彬1600',
+            num: '33,039',
+          },
+        ],
+      },
+      myData2: {
+        icon: icon2,
+        name: '生活污水排水总量',
+        value: 256256,
+        rate: 8,
+      },
+    };
   },
 };
 </script>
@@ -21,6 +55,11 @@ export default {
     position: relative;
     width: 393px;
     height: 444px;
-    padding-top: 20px;
+    padding: 20px 0;
+    display: flex;
+    flex-wrap: wrap;
+    .item-last {
+      align-self: flex-end;
+    }
   }
 </style>
